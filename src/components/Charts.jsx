8aaa1a3f37c04718dd37ef41c43c565b8f725553
Gerald <geraldbustosm@gmail.com>
2024-08-31 15:05:17 -0400

    add charts

--- src/components/Charts.jsx
+++ src/components/Charts.jsx
@@ -1,31 +1,69 @@
+import { useEffect, useState } from 'react';
+import { environment } from '../environment';
+import 'chart.js/auto';
+import { Line } from 'react-chartjs-2';
+
 export const Charts = () => {
 
+  const [chartData, setChartData] = useState({});
+  const [loading, setLoading] = useState([]);
+
+  const onInit = async() => {
+    try {
+      let response = await fetch(`${environment.baseUrl}/puffs?group=true`);
+
+      if (!response.ok) {
+          setLoading(false);
+          throw new Error('Ocurrió un error ' + response.statusText);
+      }
+
+      let responseData = await response.json();
+
+      responseData.result = responseData.result.reverse();
+
+      const info = {
+        labels: responseData.result.map(d => d._id),
+        datasets: [{
+          label: 'Cantidad de cigarros',
+          data: responseData.result.map(d => d.count),
+        }]
+      };
+
+      setChartData(info);
+      setLoading(false);
+    }catch (error) {
+        setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    onInit();
+  }, []);
+
     return <>
-        <div className="row mt-5">
-      <div className="col">
-        <h1 className="text-secondary">Gráficos 📊</h1>
+      <div className="row mt-5">
+        <div className="col">
+          <h1 className="text-secondary">Gráficos 📊</h1>
+        </div>
       </div>
-    </div>
-    <div className="row mt-5">
-    <table className="table">
-  <thead>
-    <tr>
-      <th scope="col">Fecha</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <td>Mark</td>
-    </tr>
-    <tr>
-      <td>Jacob</td>
-    </tr>
-    <tr>
-      <td>@twitter</td>
-    </tr>
-  </tbody>
-</table>
-    </div>
+      <div className="row mt-5">
+        <div className="chart-container">
+          {!loading && chartData.datasets ? <Line
+            data={ chartData }
+            options={{
+              plugins: {
+                title: {
+                  display: true,
+                  text: 'Cantidad de cigarros por día'
+                },
+                legend: {
+                  display: false
+                }
+              }
+            }}
+          /> : <div className='text-center'><div className="spinner-border" role="status"></div></div>}
+        </div>
+      </div>
     </>
 
 };
